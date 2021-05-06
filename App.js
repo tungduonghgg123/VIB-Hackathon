99726d17e131fd5884e5a5f0f5d629223e39098a
vu tung duong <tungduonghgg123@gmail.com>
2021-05-06 22:13:58 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text, View} from 'react-native';
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={{flex: 1}}>
+        <Text>ahihi</Text>
+      </View>
+    </NavigationContainer>
+  );
+}
