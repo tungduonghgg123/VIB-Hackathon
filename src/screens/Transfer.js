20f5e5d21fd5c3a310674b7a97a6470376d95573
vu tung duong <tungduonghgg123@gmail.com>
2021-05-07 15:01:49 +0700

    Add Searchbar

--- src/screens/Transfer.js
+++ src/screens/Transfer.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Text, View} from 'react-native';
 import IconText from '../components/icons/IconText';
+import {SearchBar} from 'react-native-elements';
+
 const Transfer = () => {
+  const [search, setSearch] = useState('');
   return (
     <View style={styles.container}>
       <View style={styles.transferOptionsContainer}>
         <IconText iconName="account-balance" text="Trong VIB" />
         <IconText iconName="account-balance" text="Ngoài VIB" />
         <IconText iconName="account-balance" text="Đầu tư" />
+      </View>
+      <View style={styles.searchBarWrapper}>
+        <SearchBar
+          placeholder="Tìm kiếm người thụ hưởng"
+          value={search}
+          onChangeText={setSearch}
+          containerStyle={styles.searchBarContainer}
+          inputContainerStyle={styles.searchBarInputContainer}
+          lightTheme={true}
+        />
       </View>
     </View>
   );
@@ -22,6 +35,23 @@ const styles = {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
+  },
+  searchBarWrapper: {
+    flex: 0.1,
+    backgroundColor: 'red',
+  },
+  searchBarContainer: {
+    backgroundColor: 'white',
+    borderColor: 'white',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  searchBarInputContainer: {
+    backgroundColor: '#F2F2F2',
+    // WHY 15
+    borderRadius: 15,
+    margin: 20,
+    marginVertical: 20,
   },
 };
 export default Transfer;
