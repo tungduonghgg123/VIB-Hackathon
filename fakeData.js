1f7281cf2133a1fdeb9de10a9088dda53e65f2dd
vu tung duong <tungduonghgg123@gmail.com>
2021-05-07 17:04:21 +0700

    Add categories data

---
+++ fakeData.js
@@ -0,0 +1,98 @@
+export default [
+  {
+    name: 'Ăn uống',
+    iconName: 'local-pizza',
+    subCategories: [
+      {
+        name: 'Nhà hàng',
+        iconName: 'restaurant-menu',
+        contacts: [
+          {
+            nickname: 'Đô ăn Hàn Quốc',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+          {
+            nickname: 'Bún chả nghĩa tân',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+          {
+            nickname: 'Cơm chay chùa láng',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+        ],
+      },
+      {
+        name: 'Cafe',
+        iconName: 'local-cafe',
+        contacts: [
+          {
+            nickname: 'The coffee house',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Mua sắm',
+    iconName: 'shopping-cart',
+    subCategories: [
+      {
+        name: 'Quần áo',
+        iconName: 'restaurant-menu',
+        contacts: [
+          {
+            nickname: 'Đô ăn Hàn Quốc',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+          {
+            nickname: 'Bún chả nghĩa tân',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+          {
+            nickname: 'Cơm chay chùa láng',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+        ],
+      },
+      {
+        name: 'Giày dép',
+        iconName: 'local-cafe',
+        contacts: [
+          {
+            nickname: 'The coffee house',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+        ],
+      },
+      {
+        name: 'Phụ kiện',
+        iconName: 'local-cafe',
+        contacts: [
+          {
+            nickname: 'The coffee house',
+            realName: 'NGUYEN THUY NGOC',
+            accountNumber: '004954054',
+            bank: 'NH TMCP KY THUONG (TECHCOMBANK)',
+          },
+        ],
+      },
+    ],
+  },
+];
