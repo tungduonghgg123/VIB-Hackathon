5746c992a09584058812397c1fa6af6416bc7ed9
vu tung duong <tungduonghgg123@gmail.com>
2021-05-08 22:31:30 +0700

    Make TransactionDetail screen
    Weird reason so I have to use balanceContainer which includes inputAmountContainer

--- src/screens/TransactionDetail/TransactionDetail.js
+++ src/screens/TransactionDetail/TransactionDetail.js
@@ -1,14 +1,49 @@
 import React, {useEffect, useState} from 'react';
-import {ScrollView, View} from 'react-native';
-import Tabs from '../../components/Tabs';
-import {Input} from 'react-native-elements';
-import VIBButton from '../../components/VIBButton';
+import {ScrollView, View, TextInput} from 'react-native';
 import {SafeAreaView} from 'react-native';
+import Contact from '../../components/icons/Contact';
+import Card from '../../components/Card';
+import IconText from '../../components/icons/IconText';
 
 const TransactionDetail = ({route}) => {
   return (
     <SafeAreaView style={styles.container}>
-      <View />
+      <ScrollView
+        contentContainerStyle={{flexGrow: 1}}
+        bounces={false}
+        keyboardShouldPersistTaps="handled">
+        <Card style={styles.contactContainer}>
+          <Contact
+            realName="VU THI QUYNH HUONG"
+            accountNumber="23102000"
+            subTitleStyle={styles.contact}
+            iconSize={31}
+            disabled={true}
+          />
+        </Card>
+        <Card style={styles.inputAmountContainer}>
+          <TextInput
+            style={styles.text}
+            placeholder="Nhập số tiền"
+            keyboardType="numeric"
+          />
+        </Card>
+        <Card style={styles.balanceContainer}>
+          <IconText color="black" iconName="account-balance-wallet" />
+        </Card>
+        <Card style={styles.inputAmountContainer}>
+          <TextInput
+            style={styles.text}
+            placeholder="Nội dung (không bắt buộc)"
+          />
+        </Card>
+        <Card style={styles.inputAmountContainer}>
+          <TextInput
+            style={styles.text}
+            placeholder="Ghi chú (không bắt buộc)"
+          />
+        </Card>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -17,18 +52,29 @@ const styles = {
     flex: 1,
     backgroundColor: '#0066B3',
   },
-  transferContainer: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    paddingTop: 10,
-    margin: 11,
+  contactContainer: {
+    flex: 0.1,
+    paddingLeft: 11,
+    paddingTop: 0,
   },
-  separater: {
-    height: 8,
-    backgroundColor: '#F2F2F2',
-    marginBottom: 15,
+  inputAmountContainer: {
+    flex: 0.08,
+    paddingLeft: 31,
+    paddingTop: 0,
+    justifyContent: 'center',
   },
-  input: {fontSize: 14},
-  inputLabel: {fontSize: 12, fontWeight: '400'},
+  balanceContainer: {
+    flex: 0.08,
+    paddingLeft: 31,
+    paddingTop: 0,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  contact: {
+    fontSize: 15,
+  },
+  text: {
+    fontSize: 13,
+  },
 };
 export default TransactionDetail;
