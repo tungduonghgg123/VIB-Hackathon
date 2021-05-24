d356c1cb404841fd2408f1d63ba8c2a97121d6a0
vu tung duong <tungduonghgg123@gmail.com>
2021-05-24 17:44:11 +0700

    clean Apollo client

--- src/model/index.js
+++ src/model/index.js
@@ -1,5 +1,5 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
-import {gql, createHttpLink} from '@apollo/client';
+import {createHttpLink} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
 import {getData} from '../helper/asyncStorage';
 
@@ -10,7 +10,6 @@ const httpLink = createHttpLink({
 const authLink = setContext(async (_, {headers}) => {
   // get the authentication token from local storage if it exists
   const userid = await getData('userid');
-  console.log('userid', userid);
   // return the headers to the context so httpLink can read them
   return {
     headers: {
@@ -23,17 +22,4 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-client
-  .query({
-    query: gql`
-      query GetRates {
-        user {
-          id
-          realName
-        }
-      }
-    `,
-  })
-  .then(result => console.log(result))
-  .catch(e => console.log(e));
 export default client;
