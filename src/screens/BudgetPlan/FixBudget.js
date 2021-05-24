869930d2bf704ab794d869a4dd81ab435ed04c2a
vu tung duong <tungduonghgg123@gmail.com>
2021-05-24 21:38:24 +0700

    format

--- src/screens/BudgetPlan/FixBudget.js
+++ src/screens/BudgetPlan/FixBudget.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import { ScrollView, View, TextInput, Text } from "react-native";
-import { SafeAreaView } from "react-native";
-import { Icon } from "react-native-elements";
-import * as Progress from "react-native-progress";
-import IconText from "../../components/icons/IconText";
-import CheckBox from "@react-native-community/checkbox";
-import DropDownPicker from "react-native-dropdown-picker";
-import { Button } from "react-native-elements/dist/buttons/Button";
+import React, {useState} from 'react';
+import {ScrollView, View, TextInput, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import {Icon} from 'react-native-elements';
+import * as Progress from 'react-native-progress';
+import IconText from '../../components/icons/IconText';
+import CheckBox from '@react-native-community/checkbox';
+import DropDownPicker from 'react-native-dropdown-picker';
+import {Button} from 'react-native-elements/dist/buttons/Button';
 
-const FixBudget = ({ route, navigation }) => {
+const FixBudget = ({route, navigation}) => {
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    { label: "Tháng", value: "month" },
-    { label: "Tuần", value: "week" },
-    { label: "Ngày", value: "day" },
+    {label: 'Tháng', value: 'month'},
+    {label: 'Tuần', value: 'week'},
+    {label: 'Ngày', value: 'day'},
   ]);
   const [startDate, setStartDate] = useState(new Date());
 
@@ -25,6 +25,7 @@ const FixBudget = ({ route, navigation }) => {
         <View style={styles.calendarFilter}>
           <DropDownPicker
             open={open}
+            disabled={true}
             value={value}
             items={items}
             setOpen={setOpen}
@@ -32,10 +33,10 @@ const FixBudget = ({ route, navigation }) => {
             setItems={setItems}
             placeholder="Tháng"
             dropDownContainerStyle={{
-              borderColor: "#0066B3",
+              borderColor: '#0066B3',
             }}
             style={{
-              borderColor: "#0066B3",
+              borderColor: '#0066B3',
               height: 30,
             }}
           />
@@ -44,14 +45,14 @@ const FixBudget = ({ route, navigation }) => {
       <View style={styles.transferContainer}>
         <View style={styles.root}>
           <View style={styles.contentHeader}>
-            <Text style={{ fontSize: 15 }}>Số chi tiêu cố định</Text>
+            <Text style={{fontSize: 15}}>Số chi tiêu cố định</Text>
             <Text style={styles.contentNumber}>2</Text>
           </View>
           <View style={styles.progressBar}>
             <Progress.Bar progress={1} width={330} color="#FAA934" />
             <Text style={styles.textReminder}>
-              {" "}
-              Tháng này bạn đã trả hết các chi tiêu cố định rồi!{" "}
+              {' '}
+              Tháng này bạn đã trả hết các chi tiêu cố định rồi!{' '}
             </Text>
           </View>
           <View style={styles.tickBoxList}>
@@ -60,9 +61,9 @@ const FixBudget = ({ route, navigation }) => {
                 <CheckBox
                   disabled={false}
                   value={toggleCheckBox}
-                  onValueChange={(newValue) => setToggleCheckBox(newValue)}
-                  style={{ width: 15, height: 15 }}
-                  boxType={"square"}
+                  onValueChange={newValue => setToggleCheckBox(newValue)}
+                  style={{width: 15, height: 15}}
+                  boxType={'square'}
                 />
               </View>
               <IconText
@@ -78,9 +79,9 @@ const FixBudget = ({ route, navigation }) => {
                 <CheckBox
                   disabled={false}
                   value={toggleCheckBox}
-                  onValueChange={(newValue) => setToggleCheckBox(newValue)}
-                  style={{ width: 15, height: 15 }}
-                  boxType={"square"}
+                  onValueChange={newValue => setToggleCheckBox(newValue)}
+                  style={{width: 15, height: 15}}
+                  boxType={'square'}
                 />
               </View>
               <IconText
@@ -92,21 +93,24 @@ const FixBudget = ({ route, navigation }) => {
               <Text style={styles.textMoney}>200.000 VNĐ</Text>
             </View>
           </View>
-          <View style={{ height: 50, marginTop: 16 }}>
+          <View style={{height: 50, marginTop: 16}}>
             <View style={styles.addButtonBox}>
               <Button
                 buttonStyle={styles.addButton}
                 title="Thêm khoản khác +"
-                titleStyle={{ fontSize: 12, color: "black" }}
+                titleStyle={{fontSize: 12, color: 'black'}}
                 type="outline"
-                containerStyle={{ borderColor: "#0066B3", borderWidth: 1 }}
-              ></Button>
+                containerStyle={{
+                  borderColor: '#0066B3',
+                  borderWidth: 1,
+                }}
+              />
               <Button
-                buttonStyle={[styles.addButton, { backgroundColor: "#0066B3" }]}
+                buttonStyle={[styles.addButton, {backgroundColor: '#0066B3'}]}
                 title="Đặt lịch gửi tiền định kỳ"
-                titleStyle={{ fontSize: 12, color: "white" }}
+                titleStyle={{fontSize: 12, color: 'white'}}
                 type="solid"
-              ></Button>
+              />
             </View>
           </View>
         </View>
@@ -117,7 +121,7 @@ const FixBudget = ({ route, navigation }) => {
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   transferContainer: {
     margin: 30,
@@ -126,38 +130,38 @@ const styles = {
     marginTop: -10,
   },
   root: {
-    flexDirection: "column",
+    flexDirection: 'column',
     flex: 1,
   },
   progressBar: {
     paddingTop: 10,
-    alignItems: "center",
+    alignItems: 'center',
   },
   textReminder: {
     fontSize: 11,
     marginTop: 3,
-    color: "gray",
+    color: 'gray',
   },
   contentNumber: {
-    color: "#1890FF",
-    fontWeight: "bold",
+    color: '#1890FF',
+    fontWeight: 'bold',
     fontSize: 20,
   },
   contentHeader: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingTop: 16,
-    alignItems: "center",
+    alignItems: 'center',
   },
   tickBoxList: {
     marginTop: 5,
   },
   tickBox: {
-    backgroundColor: "#F2F2F2",
-    alignItems: "center",
+    backgroundColor: '#F2F2F2',
+    alignItems: 'center',
     height: 44,
     borderRadius: 5,
-    flexDirection: "row",
+    flexDirection: 'row',
     marginTop: 10,
   },
   tickBoxDetail: {
@@ -167,29 +171,29 @@ const styles = {
     paddingRight: 30,
   },
   textMoney: {
-    textAlign: "right",
+    textAlign: 'right',
     flex: 1,
-    color: "#0066B3",
+    color: '#0066B3',
     marginRight: 10,
   },
   calendarFilterBox: {
     height: 50,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     zIndex: 2,
   },
   calendarFilter: {
     width: 100,
   },
   addButtonBox: {
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
   },
   addButton: {
     width: 150,
     height: 40,
-    borderColor: "blue",
+    borderColor: 'blue',
   },
 };
 export default FixBudget;
